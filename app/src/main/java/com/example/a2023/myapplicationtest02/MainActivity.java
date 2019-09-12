9bdeebab352c03ce965f3365d2f452dda7092565
Dylanzlm <1753834225@qq.com>
2019-09-12 11:31:31 +0800

    sss

--- app/src/main/java/com/example/a2023/myapplicationtest02/MainActivity.java
+++ app/src/main/java/com/example/a2023/myapplicationtest02/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.a2023.myapplicationtest02;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button=findViewById(R.id.button);
-        button.setOnClickListener();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"sssfs",Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
