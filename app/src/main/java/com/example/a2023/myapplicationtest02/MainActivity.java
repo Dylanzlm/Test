dd90242d87722869d47b2347973aacbdfb9ea86e
Dylanzlm <17538342252@qq.com>
2019-09-17 12:20:12 +0800

    2019/9/17

--- app/src/main/java/com/example/a2023/myapplicationtest02/MainActivity.java
+++ app/src/main/java/com/example/a2023/myapplicationtest02/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this,"sssfs",Toast.LENGTH_LONG).show();
                 Toast.makeText(MainActivity.this,"aaaaa",Toast.LENGTH_LONG).show();
                 Toast.makeText(MainActivity.this,"aaaaa",Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this,"aaaaa",Toast.LENGTH_LONG).show();
             }
         });
 
